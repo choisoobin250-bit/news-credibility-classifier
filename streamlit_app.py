831be89931635b3d0cb9b450d6f101683f80a552
choisoobin250-bit <choisoobin250@gmail.com>
2026-09-10 00:44:37 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -216,7 +216,9 @@ with st.sidebar:
     # Sidebar Caption
     st.markdown("""
     <div class="sidebar-caption">
-        The News Credibility Classifier can make errors. Always double-check with your own careful reading and judgment.
+        The News Credibility Classifier can make errors.<br>
+        Always double-check with your<br>
+        own careful reading and judgment.
     </div>
     """, unsafe_allow_html=True)
 
